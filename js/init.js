4a5ac4b6a8228145cca4ebcff545fddf0e969afb
scidenEffect <muwanguzishafiq923@gmail.com>
2025-06-30 18:47:26 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -573,43 +573,46 @@ function grax_tm_data_images(){
 // -----------------------------------------------------
 // ----------------    CONTACT FORM    -----------------
 // -----------------------------------------------------
-	function grax_tm_contact_form() {
-  "use strict";
-
-  jQuery(".contact_form #send_message").on('click', function (e) {
-    e.preventDefault();
-
-    var name = jQuery(".contact_form #name").val();
-    var email = jQuery(".contact_form #email").val();
-    var message = jQuery(".contact_form #message").val();
-    var success = jQuery(".contact_form .returnmessage").data('success');
-
-    jQuery(".contact_form .returnmessage").empty();
-
-    if (name === '' || email === '' || message === '') {
-      jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
-    } else {
-      jQuery.ajax({
-        url: "https://formspree.io/f/mrbkjkkp",
-        method: "POST",
-        data: {
-          name: name,
-          email: email,
-          message: message
-        },
-        dataType: "json",
-        success: function () {
-          jQuery(".contact_form .returnmessage").append("<span class='contact_success'>" + success + "</span>");
-          jQuery(".contact_form .returnmessage").slideDown(500).delay(4000).slideUp(500);
-          jQuery("#contact_form")[0].reset();
-        },
-        error: function () {
-          jQuery(".contact_form .returnmessage").append("<span class='contact_error'>There was an error submitting the form. Please try again later.</span>");
-          jQuery(".contact_form .returnmessage").slideDown(500).delay(4000).slideUp(500);
-        }
-      });
-    }
-  });
+function grax_tm_contact_form(){
+	
+	"use strict";
+	
+	jQuery(".contact_form #send_message").on('click', function(){
+		
+		var name 		= jQuery(".contact_form #name").val();
+		var email 		= jQuery(".contact_form #email").val();
+		var message 	= jQuery(".contact_form #message").val();
+		var subject 	= jQuery(".contact_form #subject").val();
+		var success     = jQuery(".contact_form .returnmessage").data('success');
+	
+		jQuery(".contact_form .returnmessage").empty(); //To empty previous error/success message.
+		//checking for blank fields	
+		if(name===''||email===''||message===''){
+			
+			jQuery('div.empty_notice').slideDown(500).delay(2000).slideUp(500);
+		}
+		else{
+			// Returns successful data submission message when the entered information is stored in database.
+			jQuery.post("modal/contact.php",{ ajax_name: name, ajax_email: email, ajax_message:message, ajax_subject: subject}, function(data) {
+				
+				jQuery(".contact_form .returnmessage").append(data);//Append returned message to message paragraph
+				
+				
+				if(jQuery(".contact_form .returnmessage span.contact_error").length){
+					jQuery(".contact_form .returnmessage").slideDown(500).delay(2000).slideUp(500);		
+				}else{
+					jQuery(".contact_form .returnmessage").append("<span class='contact_success'>"+ success +"</span>");
+					jQuery(".contact_form .returnmessage").slideDown(500).delay(4000).slideUp(500);
+				}
+				
+				if(data===""){
+					jQuery("#contact_form")[0].reset();//To reset form fields on success
+				}
+				
+			});
+		}
+		return false; 
+	});
 }
 
 // -----------------------------------------------------
